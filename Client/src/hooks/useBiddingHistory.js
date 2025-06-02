e8e72891c8d0036b49496c61982c8c0e1287e69d
Muhammad Umair Shad <151988665+shad-developer@users.noreply.github.com>
2025-06-02 11:34:10 +0500

    ]useBiddingHistory.js

--- Client/src/hooks/useBiddingHistory.js
+++ Client/src/hooks/useBiddingHistory.js
@@ -11,7 +11,7 @@ const useBiddingHistory = () => {
   }, [dispatch]);
 
   // Group bidding histories by product ID
-  const groupedHistories = histories.reduce((acc, bid) => {
+  const groupedHistories = histories?.reduce((acc, bid) => {
     const productId = bid.product.toString();
     if (!acc[productId]) acc[productId] = [];
     acc[productId].push(bid);
@@ -22,7 +22,7 @@ const useBiddingHistory = () => {
   const highestBid = (productId) => {
     const bids = groupedHistories[productId];
     if (!bids) return 0;
-    return Math.max(...bids.map((bid) => bid.price));
+    return Math.max(...bids?.map((bid) => bid.price));
   };
 
  // Get total number of bids for a product
