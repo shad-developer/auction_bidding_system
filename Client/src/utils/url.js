2207a54b084a5114cc8a8bf06227c5df8a570ef0
Muhammad Umair Shad <151988665+shad-developer@users.noreply.github.com>
2025-06-02 11:12:45 +0500

    url.js

--- Client/src/utils/url.js
+++ Client/src/utils/url.js
@@ -1,1 +1,2 @@
-export const BACKEND_URL = "http://localhost:5000/api";
+// export const BACKEND_URL = "http://localhost:5000/api";
+export const BACKEND_URL = "https://auction-bidding-system.onrender.com/api";
